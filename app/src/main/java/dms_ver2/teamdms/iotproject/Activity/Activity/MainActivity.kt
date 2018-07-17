fe2b92d00a9753be417d13bdeca88beaacd2c409
LeeSungHyun <joooosan011@gmail.com>
2018-07-17 11:55:53 +0900

    fix untracked file

---
+++ app/src/main/java/dms_ver2/teamdms/iotproject/Activity/Activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package dms_ver2.teamdms.iotproject.Activity.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import dms_ver2.teamdms.iotproject.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
